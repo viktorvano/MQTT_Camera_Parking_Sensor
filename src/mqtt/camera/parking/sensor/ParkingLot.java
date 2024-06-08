49b9b5f56ac601ef752dbcbb7b9c244797972013
Viktor Vaňo <viktorvano@gmail.com>
2024-06-08 16:43:24 +0200

    calculateIfParkingLotIsFree

--- src/mqtt/camera/parking/sensor/ParkingLot.java
+++ src/mqtt/camera/parking/sensor/ParkingLot.java
@@ -1,16 +1,53 @@
 package mqtt.camera.parking.sensor;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class ParkingLot {
-    public ArrayList<PixelPlace> pixelPlaces = new ArrayList<>();
+    public ArrayList<PixelPlace> pixelPlaces;
     public String name;
-    public boolean isFree = false;
+    private boolean isFree = false;
 
     public ParkingLot(String name, ArrayList<PixelPlace> pixelPlaces)
     {
         this.name = name;
         this.pixelPlaces = pixelPlaces;
+    }
+
+    public void calculateIfParkingLotIsFree(BufferedImage bufferedImage, int tolerance)
+    {
+        boolean free = true;
+        int averageColor;
+        Color c;
+        int red, green, blue;
+        for(PixelPlace place : pixelPlaces)
+        {
+            // verify if all the pixels are grey within the tolerance
+            c = new Color(bufferedImage.getRGB(place.x, place.y));
+            red = c.getRed();
+            green = c.getGreen();
+            blue = c.getBlue();
+            averageColor = (red + green + blue) / 3;
+            int min = averageColor - tolerance;
+            int max = averageColor + tolerance;
+
+            // if any of these colors are off the tolerance range the parking lot if not free
+            if(red > max || red < min
+            || green > max || green < min
+            || blue > max || blue < min)
+            {
+                free = false;
+                break;
+            }
+        }
+
+        this.isFree = free;
+    }
+
+    public boolean isFree()
+    {
+        return isFree;
     }
 
     @Override
